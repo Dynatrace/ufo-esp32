9dc1bb779f4d528d32d6d8fed2c7e907d24173e1
hzeitlhofer <harald.zeitlhofer@dynatrace.com>
2017-07-21 14:02:44 +0200

    github URL for firmware changed

--- main/Ota.h
+++ main/Ota.h
@@ -13,8 +13,10 @@
 #include "WebClient.h"
 
 //#define LATEST_FIRMWARE_URL "https://surpro4:9999/getfirmware"  // testing with local go server
-#define OTA_LATEST_FIRMWARE_JSON_URL "https://github.com/Dynatrace/ufo-esp32/raw/master/firmware/version.json"
-#define OTA_LATEST_FIRMWARE_URL "https://github.com/Dynatrace/ufo-esp32/raw/master/firmware/ufo-esp32.bin"
+//#define OTA_LATEST_FIRMWARE_JSON_URL "https://github.com/Dynatrace/ufo-esp32/raw/master/firmware/version.json"
+//#define OTA_LATEST_FIRMWARE_URL "https://github.com/Dynatrace/ufo-esp32/raw/master/firmware/ufo-esp32.bin"
+#define OTA_LATEST_FIRMWARE_JSON_URL "https://raw.githubusercontent.com/Dynatrace/ufo-esp32/master/firmware/version.json"
+#define OTA_LATEST_FIRMWARE_URL "https://raw.githubusercontent.com/Dynatrace/ufo-esp32/master/firmware/ufo-esp32.bin"
 
 
 #define OTA_PROGRESS_NOTYETSTARTED      -1
